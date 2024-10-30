0f82eb92d81a5787c35dc66cb5d371aef364de23
Nasakin Artem <artnas379@gmail.com>
2024-10-30 19:16:01 +0300

    feat: add docstring

--- app/services/yandex_disk.py
+++ app/services/yandex_disk.py
@@ -5,7 +5,18 @@ from app.config import YANDEX_TOKEN
 
 yadisk_instance = yadisk.YaDisk(token=YANDEX_TOKEN)
 
-def upload_photo(file_stream, file_id):
+def upload_photo(file_stream, file_id) -> bool:
+    """
+    Загружает фотографию на Яндекс.Диск.
+
+    :param file_stream: Поток байтов, содержащий фотографию.
+    :type file_stream: BytesIO
+    :param file_id: Уникальный идентификатор файла.
+    :type file_id: str
+    :return: True, если загрузка прошла успешно, иначе False.
+    :rtype: bool
+    """
+
     try:
         yadisk_instance.upload(file_stream, f"/bot_uploads/{file_id}", overwrite=True)
         return True
